a7232f30243007052857811e383bfbcf56a33e98
khj2373 <xomg2373@gmail.com>
2024-11-19 16:47:38 +0900

    add: 사이트 관리자 interceptor작성

--- src/main/java/com/kh/sportsmate/common/intercepter/LoginInterceptor.java
+++ src/main/java/com/kh/sportsmate/common/intercepter/LoginInterceptor.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpSession;
  * 2024. 11. 17.        jun       최초 생성
  */
 @Component
-public class loginIntercepter implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
