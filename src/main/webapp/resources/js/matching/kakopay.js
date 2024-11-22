3edda372e971edbacedd1678c3af5dd8b6726a4b
khj2373 <xomg2373@gmail.com>
2024-11-22 09:07:16 +0900

    update: 카카오페이 키값 변수화

---
+++ src/main/webapp/resources/js/matching/kakopay.js
@@ -0,0 +1,25 @@
+// 카카오페이 결제 팝업창 연결
+$(function() {
+    const name = document.querySelector('');
+    const price = document.querySelector('');
+    $("#btn-pay-ready").click(function(e) {
+        // 아래 데이터 외에도 필요한 데이터를 원하는 대로 담고, Controller에서 @RequestBody로 받으면 됨
+        let data = {
+            name: name,    // 카카오페이에 보낼 대표 상품명
+            totalPrice: price // 총 결제금액
+        };
+        
+        $.ajax({
+            type: 'POST',
+            url: 'payMatch.st',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function(response) {
+                location.href = response.next_redirect_pc_url;
+            },
+            error: function(){
+                console.log("결제실패");
+            }
+        });
+    });
+});
