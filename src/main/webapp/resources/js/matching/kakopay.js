326386275339902b66084b3bf2c68702bd8f7c68
khj2373 <xomg2373@gmail.com>
2024-11-25 17:49:31 +0900

    카카오페이 결제 전 페이지

--- src/main/webapp/resources/js/matching/kakopay.js
+++ src/main/webapp/resources/js/matching/kakopay.js
@@ -23,3 +23,29 @@ $(function() {
         });
     });
 });
+
+//모바일
+$(function() {
+    const name = document.querySelector('');
+    const price = document.querySelector('');
+    $("#btn-mobile-ready").click(function(e) {
+        // 아래 데이터 외에도 필요한 데이터를 원하는 대로 담고, Controller에서 @RequestBody로 받으면 됨
+        let data = {
+            name: name,    // 카카오페이에 보낼 대표 상품명
+            totalPrice: price // 총 결제금액
+        };
+        
+        $.ajax({
+            type: 'POST',
+            url: 'payMatch.st',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function(response) {
+                location.href = response.next_redirect_mobile_url;
+            },
+            error: function(){
+                console.log("결제실패");
+            }
+        });
+    });
+});
